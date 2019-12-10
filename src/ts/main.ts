411bb757142711fb30f3643e7a3c8bf879c5bdfd
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-12-10 19:36:27 +0100

    Add count down timers

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,9 @@
 import { registerDropdownListener } from "./dropdown";
 import { registerHamburgerListener } from "./hamburger";
+import { initTimers } from "./timer";
 
 window.onload = function() {
   registerDropdownListener();
   registerHamburgerListener();
+  initTimers();
 }
